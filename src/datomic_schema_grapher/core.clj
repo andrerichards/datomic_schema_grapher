7f166d4cd2483d71f12212f7b9f209ad7f83507e
Andre Richards <andrer@hti-systems.com>
2018-11-09 01:16:15 +0200

    Changed graph-datomic to multi-arity function, so you can either call it with uri or db. This allows usage in other contexts e.g. Datomic Cloud

--- src/datomic_schema_grapher/core.clj
+++ src/datomic_schema_grapher/core.clj
@@ -6,15 +6,17 @@
 
 (defn graph-datomic
   "Render a nice graphviz of your Datomic schema"
-  [uri & {:keys [save-as no-display exit-on-close]}]
-  (let [db (d/db (d/connect uri))
-        schema (schema db)
-        references (references db)]
-    (if save-as
-      (spit save-as (to-dot schema references)))
-    (when-not no-display
-      (let [jframe (show schema references)]
-        (when exit-on-close
-          ;; mainly for use with the lein plugin
-          (.setDefaultCloseOperation jframe JFrame/EXIT_ON_CLOSE)
-          (while true (Thread/sleep 500)))))))
+  ([uri & options]
+   (let [db (d/db (d/connect uri)) ]
+     (graph-datomic db options)))
+  ([db & {:keys [save-as no-display exit-on-close]}]
+   (let [schema (schema db)
+         references (references db)]
+     (if save-as
+       (spit save-as (to-dot schema references)))
+     (when-not no-display
+       (let [jframe (show schema references)]
+         (when exit-on-close
+           ;; mainly for use with the lein plugin
+           (.setDefaultCloseOperation jframe JFrame/EXIT_ON_CLOSE)
+           (while true (Thread/sleep 500))))))))
